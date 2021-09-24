8a1c6de4134b0eec64f4bbdbde6f6d3917a0e862
sergiosalguero <salgueroflor1982@gmail.com>
2021-09-24 15:09:18 +0200

    create nav, login and auth

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,62 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
+import SignUpPage from "../views/SignUpPage.vue";
+import AlbumDetailPage from "../views/AlbumDetailPage.vue";
+import AlbumsPage from "../views/AlbumsPage.vue";
+import { Auth } from "aws-amplify";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: "/",
+    name: "Home",
+    component: Home,
   },
   {
-    path: '/about',
-    name: 'About',
+    path: "/signup",
+    name: "SignUpPage",
+    component: SignUpPage,
+  },
+  {
+    path: "/album/:id",
+    name: "AlbumsDetailPage",
+    component: AlbumDetailPage,
+    meta: { requiresAuth: true },
+  },
+  {
+    path: "/albums",
+    name: "AlbumsPage",
+    component: AlbumsPage,
+    meta: { requiresAuth: true },
+  },
+  {
+    path: "/about",
+    name: "About",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+  },
+];
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
-  routes
-})
+  routes,
+});
 
-export default router
+router.beforeEach(async (to, from, next) => {
+  const requiresAuth = to.matched.some((record) => record.meta.requiresAuth);
+  const isAuthenticated = await Auth.currentUserInfo();
+
+  if (requiresAuth && !isAuthenticated) {
+    next("/");
+  } else {
+    next();
+  }
+});
+
+export default router;
